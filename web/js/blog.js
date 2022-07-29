171ff67372c5e698ba376e306b4da1bcda55dd30
Hosein <hossein@linquip.com>
2022-07-29 21:21:59 +0430

    check offline/online mode switch based on events

--- web/js/blog.js
+++ web/js/blog.js
@@ -1,16 +1,31 @@
-(function Blog(){
+(function Blog() {
 	"use strict";
 
 	var offlineIcon;
+	var isOnline = ("onLine" in navigator) ? navigator.onLine : true;
 	var isLoggedIn = /isLoggedIn=1/.test(document.cookie.toString() || "");
 
-	document.addEventListener("DOMContentLoaded",ready,false);
+	document.addEventListener("DOMContentLoaded", ready, false);
 
 
 	// **********************************
 
 	function ready() {
 		offlineIcon = document.getElementById("connectivity-status");
+
+		if (!isOnline) {
+			offlineIcon.classList.remove("hidden");
+		}
+
+		window.addEventListener("online", () => {
+			offlineIcon.classList.add("hidden");
+			isOnline = true;
+		});
+
+		window.addEventListener("offline", () => {
+			offlineIcon.classList.remove("hidden");
+			isOnline = false;
+		});
 	}
 
 })();
