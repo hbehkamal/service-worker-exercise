98e916bf742430086cb56b24ad182595f6cf87ee
Hosein <hossein@linquip.com>
2022-07-29 21:38:02 +0430

    init ServiceWorker

--- web/js/blog.js
+++ web/js/blog.js
@@ -4,10 +4,13 @@
 	var offlineIcon;
 	var isOnline = ("onLine" in navigator) ? navigator.onLine : true;
 	var isLoggedIn = /isLoggedIn=1/.test(document.cookie.toString() || "");
+	var usingSW = ("serviceWorker" in navigator);
+	var swRegistration;
+	var svcworker;
 
 	document.addEventListener("DOMContentLoaded", ready, false);
 
-
+	initServiceWorker().catch(console.error());
 	// **********************************
 
 	function ready() {
@@ -26,6 +29,21 @@
 			offlineIcon.classList.remove("hidden");
 			isOnline = false;
 		});
+	}
+
+	const initServiceWorker = async () => {
+		swRegistration = await navigator.serviceWorker.register("/sw.js", {
+			updateViaCache: "none",
+		});
+
+		// installing	: First time 
+		// wating     : when new SW is wating for last SW to finish its job. we can skip this step (skipWating) after installation;
+		svcworker = swRegistration.installing || swRegistration.wating || swRegistration.active;
+
+		navigator.serviceWorker.addEventListener("controllerchange", () => {
+			// new SW started working
+			svcworker = navigator.serviceWorker.controller;
+		})
 	}
 
 })();
