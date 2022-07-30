da8ee4a85833ee26029070c6cf7335ef35fc6607
Hosein <hossein@linquip.com>
2022-07-30 22:37:17 +0430

    Handle message sending & getting

--- web/js/blog.js
+++ web/js/blog.js
@@ -23,11 +23,14 @@
 		window.addEventListener("online", () => {
 			offlineIcon.classList.add("hidden");
 			isOnline = true;
+			sendStatusUpdate();
+
 		});
 
 		window.addEventListener("offline", () => {
 			offlineIcon.classList.remove("hidden");
 			isOnline = false;
+			sendStatusUpdate();
 		});
 	}
 
@@ -39,11 +42,39 @@
 		// installing	: First time 
 		// wating     : when new SW is wating for last SW to finish its job. we can skip this step (skipWating) after installation;
 		svcworker = swRegistration.installing || swRegistration.wating || swRegistration.active;
+		sendStatusUpdate(svcworker);
 
 		navigator.serviceWorker.addEventListener("controllerchange", () => {
 			// new SW started working
 			svcworker = navigator.serviceWorker.controller;
-		})
+			sendStatusUpdate(svcworker);
+		});
+
+		navigator.serviceWorker.addEventListener("message", onSWMessage);
+	}
+
+	function onSWMessage(evt) {
+		var { data } = evt;
+		if (data.requestStatusUpdate) {
+			// hey page! can u give me an update?
+			console.log("Received status update request from service worker, responding");
+			// port between page and sw
+			sendStatusUpdate(evt.ports && evt.ports[0]);
+		}
+	}
+
+	function sendStatusUpdate(target) {
+		sendSWMessage({ statusUpdate: { isOnline, isLoggedIn } }, target)
+	}
+
+	function sendSWMessage(msg, target) {
+		if (target) {
+			target.postMessage(msg);
+		} else if (svcworker) {
+			svcworker.postMessage(msg);
+		} else {
+			navigator.serviceWorker.controller.postMessage(msg);
+		}
 	}
 
 })();
--- web/js/sw.js
+++ web/js/sw.js
@@ -2,10 +2,14 @@
 
 // This file's codes are going to run at installation and at restarting (inactive bg tab becomes foreground) state of SW life cycle
 
-const version = 1; // All resources are an atomic package which might get updated
+const version = 2; // All resources are an atomic package which might get updated
+
+var isOnline = true;
+var isLoggedIn = false;
 
 self.addEventListener("install", onInstall);
 self.addEventListener("activate", onActivate);
+self.addEventListener("message", onMessage);
 
 // ********************************************
 
@@ -15,12 +19,33 @@ main().catch(console.error());
 // ********************************************
 
 async function main() {
-  console.log(`Service Worker version ${version} started`);
+  await sendMessage({ requestStatusUpdate: true })
 }
 
 async function onInstall(evt) {
   console.log(`Service Worker version ${version} installed`);
   self.skipWaiting();
+}
+
+async function sendMessage(msg) {
+  var allClients = await clients.matchAll({
+    includeUncontrolled: true
+  });
+
+  return Promise.all(
+    allClients.map(function clientMsg(client) {
+      var chan = new MessageChannel();
+      chan.port1.onmessage = onMessage;  // listen on port 1
+      return client.postMessage(msg, [chan.port2]); // Send on port 2
+    })
+  )
+}
+
+function onMessage({ data }) {
+  if (data.statusUpdate) {
+    ({ isLoggedIn, isOnline } = data.statusUpdate);
+    console.log("SW version is ", version, ", isOnline", isOnline, ", isLoggedIn", isLoggedIn);
+  }
 }
 
 async function handleActivation() {
