b7f869050678f46003019ac9f496ff76900b5e15
Hosein <hossein@linquip.com>
2022-08-13 21:20:46 +0430

    remove comments

--- web/js/blog.js
+++ web/js/blog.js
@@ -23,7 +23,6 @@
 		window.addEventListener("online", () => {
 			offlineIcon.classList.add("hidden");
 			isOnline = true;
-			// 2.5.2: Send a status update on online
 			sendStatusUpdate();
 
 		});
@@ -31,7 +30,6 @@
 		window.addEventListener("offline", () => {
 			offlineIcon.classList.remove("hidden");
 			isOnline = false;
-			// 2.5.2: Send a status update on offline
 			sendStatusUpdate();
 		});
 	}
@@ -44,54 +42,38 @@
 		// installing	: First time 
 		// wating     : when new SW is wating for last SW to finish its job. we can skip this step (skipWating) after installation;
 		svcworker = swRegistration.installing || swRegistration.wating || swRegistration.active;
-		// 2.5.0: Send a status update when service worker installed first time
 		sendStatusUpdate(svcworker);
 
 		navigator.serviceWorker.addEventListener("controllerchange", () => {
 			// new SW started working
 			svcworker = navigator.serviceWorker.controller;
-			// 2.5.1 Send a status update when service worker controller changed
 			sendStatusUpdate(svcworker);
 		});
 
-		// 2.0 : Listen for messages FROM service worker
 		navigator.serviceWorker.addEventListener("message", onSWMessage);
 	}
 
-	// 2.1 : Listen for messages (from SW) and send a status update (to SW)
 	function onSWMessage(evt) {
 		var { data } = evt;
-		// 2.2 The data sent from service worker. In this case asked for a update about isOnline & isLoggedIn
 		if (data.requestStatusUpdate) {
 			// hey page! can u give me an update?
 			console.log("Received status update request from service worker, responding");
-			// 2.3 port between page and sw 
-			// Question: Whay we sned the message to svcworker?
-			// Answer: because we might have multipe pages comunicating with
-			// and we have to comunitcate throgh MESSAGE CHANNELs (unique local set of ports)
+
 			sendStatusUpdate(evt.ports && evt.ports[0]);
 		}
 	}
 
-	// 2.4 : send something (E.G. statuses like isOnline & isLoggedIn to service worker
-	// Cause service worker DO NOT have access to coolies (isLoggedIn) and Navigator (isOnline)
 	function sendStatusUpdate(target) {
-		// The target is the PORT[0]
 		const msg = { statusUpdate: { isOnline, isLoggedIn } };
 		sendSWMessage(msg, target)
 	}
 
-	// 1 : Send A message TO Service worker from page
 	function sendSWMessage(msg, target) {
-		// The target is the PORT[0] that SW is listening from
-		// Or, is the service worker freshly (installed / changed) contrller (check 2.5)
 		if (target) {
 			target.postMessage(msg);
 		} else if (svcworker) {
-			// Used when we send a status update on online/offline to currently active service worker
 			svcworker.postMessage(msg);
 		} else {
-			// This case will not happen but it's just a fallback to make sure errors are handled.
 			navigator.serviceWorker.controller.postMessage(msg);
 		}
 	}
--- web/js/sw.js
+++ web/js/sw.js
@@ -1,7 +1,5 @@
 "use strict";
 
-// This file's codes are going to run at installation and at restarting (inactive bg tab becomes foreground) state of SW life cycle
-
 const version = 3; // All resources are an atomic package which might get updated
 
 var isOnline = true;
@@ -30,7 +28,6 @@ var urlsToCache = {
 
 self.addEventListener("install", onInstall);
 self.addEventListener("activate", onActivate);
-// 3.0 : Listen for messages from the page
 self.addEventListener("message", onMessage);
 
 // ********************************************
@@ -41,7 +38,6 @@ main().catch(console.error());
 // ********************************************
 
 async function main() {
-  // 1: Ask for a status update from the page
   await sendMessage({ requestStatusUpdate: true })
 }
 
@@ -52,27 +48,18 @@ async function onInstall(evt) {
 
 // 2.0 : send message to page
 async function sendMessage(msg) {
-  // 2.1 : get list of all service worker clients
-  // includeUncontrolled: true means:
-  // the matching operation will return all service worker clients who share the same origin as the current SW.
-  // Otherwise, it returns only the service worker clients controlled by the current service worker.
   var allClients = await clients.matchAll({
     includeUncontrolled: true
   });
 
   return Promise.all(
     allClients.map(function clientMsg(client) {
-      // 2.2 : Create a unique message channel for each client (page) that we are talking to
       var chan = new MessageChannel();
-      // 2.3 : listen from messages on FIRST created port for each channel 
       chan.port1.onmessage = onMessage;
-
-      // 2.4 : send message to the page (client) using SECOND port
       return client.postMessage(msg, [chan.port2]);
     })
   )
 }
-// 3.1 : Receive messages from page
 function onMessage({ data }) {
   if (data.statusUpdate) {
     ({ isLoggedIn, isOnline } = data.statusUpdate);
